dff5702ce9fbe0ae46d06a04095493c5711c9286
hendratay <hendratay96@gmail.com>
2018-06-08 00:56:16 +0700

    move todo from activity to taskfragment

--- app/src/main/java/com/example/user/whattodo/MainActivity.kt
+++ app/src/main/java/com/example/user/whattodo/MainActivity.kt
@@ -1,40 +1,24 @@
 package com.example.user.whattodo
 
-import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AlertDialog
-import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.EditText
 import com.example.user.whattodo.db.TodoDatabase
-import com.example.user.whattodo.db.TodoEntity
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*;
 import javax.inject.Inject
-import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     @Inject lateinit var database : TodoDatabase
-    private lateinit var adapter: TodoAdapter
-    private var TodoList: MutableList<Todo> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        App.component.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        App.component.inject(this)
-
         setupNavigation()
-        //setupRecyclerView()
-        //getTodo()
     }
 
     private fun setupNavigation() {
@@ -52,12 +36,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setupRecyclerView() {
-        rv_todo_list.layoutManager = LinearLayoutManager(this)
-        adapter = TodoAdapter(TodoList, { todo: Todo -> onItemChecked(todo)}, { todoList: List<Todo> -> deleteTodo(todoList)})
-        rv_todo_list.adapter = adapter
-    }
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
@@ -65,95 +43,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
         R.id.action_done -> {
-            doneActivity()
             true
         }
-
         else -> {
             super.onOptionsItemSelected(item)
         }
     }
 
-    fun addTodoDialog() {
-        val alert = AlertDialog.Builder(this)
-        val todoEditText = EditText(this)
-        todoEditText.hint = "Enter Todo"
-
-        alert.setTitle("Add New Todo")
-        // TODO: Use custom layout
-        alert.setView(todoEditText, 48, 48, 48, 0)
-
-        alert.setPositiveButton("Add") {
-            dialog, _ ->
-
-            database.todoDao().insertTodo(TodoEntity(todoEditText.text.toString(), false))
-            getTodo()
-
-            dialog.dismiss()
-        }
-        alert.setNegativeButton("Cancel") {
-            dialog, _ ->
-
-            dialog.dismiss()
-        }
-
-        alert.show()
-    }
-
-    fun getTodo() {
-        database.todoDao().getTodo()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe{
-                    TodoList.clear()
-                    for (todo: TodoEntity in it) {
-                        TodoList.add(Todo(todo.id, todo.todo, todo.done))
-                    }
-                    // For fixing java.lang.IndexOutOfBoundsException: Inconsistency detected
-                    rv_todo_list.recycledViewPool.clear()
-                    adapter.notifyDataSetChanged()
-                }
-    }
-
-    fun onItemChecked(todo: Todo) {
-        TodoList.remove(todo)
-        if(!rv_todo_list.isComputingLayout) {
-            adapter.notifyDataSetChanged()
-            moveTodoToDone(todo)
-            Snackbar.make(cl_main, todo.todoText + " have done", Snackbar.LENGTH_SHORT).show()
-        }
-    }
-
-    fun moveTodoToDone(todo: Todo) {
-        val entity = TodoEntity(todo.todoId, todo.todoText, true)
-        Single.fromCallable { database.todoDao().updateTodo(entity) }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe()
-    }
-
-    fun deleteTodo(todoList: List<Todo>) {
-        val backup: MutableList<TodoEntity> = ArrayList()
-        todoList.forEach {
-            val entity = TodoEntity(it.todoId, it.todoText, it.done)
-            backup.add(entity)
-            Single.fromCallable { database.todoDao().deleteTodo(entity) }
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe()
-        }
-        val snackbar = Snackbar.make(cl_main, todoList.size.toString() + " item deleted", Snackbar.LENGTH_SHORT)
-        snackbar.show()
-        snackbar.setAction("UNDO", View.OnClickListener {
-            backup.forEach {
-                database.todoDao().insertTodo(it)
-                getTodo()
-            }
-        } )
-    }
-
-    fun doneActivity() {
-        val intent = Intent(this, DoneActivity::class.java)
-        startActivity(intent)
-    }
 }
--- app/src/main/java/com/example/user/whattodo/TaskFragment.kt
+++ app/src/main/java/com/example/user/whattodo/TaskFragment.kt
@@ -1,15 +1,122 @@
 package com.example.user.whattodo
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import com.example.user.whattodo.db.TodoEntity
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_task.*
 
 class TaskFragment: Fragment() {
 
+    private lateinit var adapter: TodoAdapter
+    private var TodoList: MutableList<Todo> = ArrayList()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_task,container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setupRecyclerView()
+        getTodo()
+        (activity as MainActivity).button_add.setOnClickListener {
+            addTodoDialog()
+        }
+    }
+
+    private fun setupRecyclerView() {
+        rv_task.layoutManager = LinearLayoutManager(activity)
+        adapter = TodoAdapter(TodoList, { todo: Todo -> onItemChecked(todo)}, { todoList: List<Todo> -> deleteTodo(todoList)})
+        rv_task.adapter = adapter
+    }
+
+    private fun getTodo() {
+        (activity as MainActivity).database.todoDao().getTodo()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe{
+                    TodoList.clear()
+                    for (todo: TodoEntity in it) {
+                        TodoList.add(Todo(todo.id, todo.todo, todo.done))
+                    }
+                    // For fixing java.lang.IndexOutOfBoundsException: Inconsistency detected
+                    rv_task.recycledViewPool.clear()
+                    adapter.notifyDataSetChanged()
+                }
+    }
+
+    private fun onItemChecked(todo: Todo) {
+        TodoList.remove(todo)
+        if(!rv_task.isComputingLayout) {
+            adapter.notifyDataSetChanged()
+            moveTodoToDone(todo)
+            Snackbar.make(cl_main, todo.todoText + " have done", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun moveTodoToDone(todo: Todo) {
+        val entity = TodoEntity(todo.todoId, todo.todoText, true)
+        Single.fromCallable { (activity as MainActivity).database.todoDao().updateTodo(entity) }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe()
+    }
+
+    private fun deleteTodo(todoList: List<Todo>) {
+        val backup: MutableList<TodoEntity> = ArrayList()
+        todoList.forEach {
+            val entity = TodoEntity(it.todoId, it.todoText, it.done)
+            backup.add(entity)
+            Single.fromCallable { (activity as MainActivity).database.todoDao().deleteTodo(entity) }
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe()
+        }
+        val snackbar = Snackbar.make(cl_main, todoList.size.toString() + " item deleted", Snackbar.LENGTH_SHORT)
+        snackbar.show()
+        snackbar.setAction("UNDO") {
+            backup.forEach {
+                (activity as MainActivity).database.todoDao().insertTodo(it)
+                getTodo()
+            }
+        }
+    }
+
+    private fun addTodoDialog() {
+        val alert = AlertDialog.Builder(activity)
+        val todoEditText = EditText(activity)
+        todoEditText.hint = "Enter Todo"
+
+        alert.setTitle("Add New Todo")
+        // TODO: Use custom layout
+        //alert.setView(todoEditText, 48, 48, 48, 0)
+
+        alert.setPositiveButton("Add") {
+            dialog, _ ->
+
+            (activity as MainActivity).database.todoDao().insertTodo(TodoEntity(todoEditText.text.toString(), false))
+            getTodo()
+
+            dialog.dismiss()
+        }
+        alert.setNegativeButton("Cancel") {
+            dialog, _ ->
+
+            dialog.dismiss()
+        }
+
+        alert.show()
     }
 
 }
