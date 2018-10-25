054eaaafaf79ad32eddabc28f841820310edf5ef
hendratay <hendratay96@gmail.com>
2018-10-25 08:29:04 +0700

    sendfeedback with gmail

--- app/src/main/java/com/example/user/whattodo/activity/SendFeedbackActivity.kt
+++ app/src/main/java/com/example/user/whattodo/activity/SendFeedbackActivity.kt
@@ -2,10 +2,14 @@ package com.example.user.whattodo.activity
 
 import android.os.Bundle
 import android.support.v4.app.NavUtils
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.example.user.whattodo.BuildConfig
 import com.example.user.whattodo.R
+import com.example.user.whattodo.utils.Gmail
 import kotlinx.android.synthetic.main.activity_send_feedback.*
+import kotlin.concurrent.thread
 
 class SendFeedbackActivity : AppCompatActivity() {
 
@@ -26,6 +30,28 @@ class SendFeedbackActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         NavUtils.navigateUpFromSameTask(this)
+    }
+
+    private fun sendFeedback() {
+        val progressDialog = progressDialog()
+        thread {
+            runOnUiThread { progressDialog.show() }
+            try {
+                val sender = Gmail(BuildConfig.APP_GMAIL, BuildConfig.APP_GMAIL_PASS)
+                sender.sendMail("SUBJECT",
+                        "FEEDBACK",
+                        "FROM",
+                        BuildConfig.CUSTOMER_SERVICE_GMAIL)
+            } catch (e: Exception) {
+            }
+            runOnUiThread { progressDialog.dismiss() }
+        }
+    }
+
+    private fun progressDialog(): AlertDialog {
+        val builder = AlertDialog.Builder(this)
+        builder.setView(layoutInflater.inflate(R.layout.dialog_progress, null))
+        return builder.create()
     }
 
 }
