80d54419eb604c676007bd1a654c8aa71ea95356
hendratay <hendratay96@gmail.com>
2019-01-14 10:10:05 +0700

    remove home ui and home adapter

--- app/src/main/java/com/example/user/whattodo/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/user/whattodo/fragment/HomeFragment.kt
@@ -1,32 +1,3 @@
 package com.example.user.whattodo.fragment
 
-import android.support.v7.widget.LinearLayoutManager
-import com.example.user.whattodo.adapter.HomeAdapter
-import kotlinx.android.synthetic.main.fragment_todo.*
-import io.reactivex.disposables.CompositeDisposable
-
-class HomeFragment: TodoFragment() {
-
-    private lateinit var adapter: HomeAdapter
-    private val todoType: MutableList<String> = ArrayList()
-    private val todoCount: MutableList<Int> = ArrayList()
-    private var todoDoneCount: MutableList<Int> = ArrayList()
-
-    var compositeDisposable = CompositeDisposable()
-
-    override fun setupRecyclerView() {
-        adapter = HomeAdapter(todoType, todoCount, todoDoneCount)
-        recycler_view.layoutManager = LinearLayoutManager(requireContext())
-        recycler_view.adapter = adapter
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
-    }
-
-}
+class HomeFragment: TodoFragment()
