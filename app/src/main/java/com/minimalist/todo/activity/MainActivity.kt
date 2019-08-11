568cf64619b20918bedcacda376b37a1060421d4
hendratay <hendratay96@gmail.com>
2019-08-11 20:09:32 +0700

    fix linking to play store

--- app/src/main/java/com/minimalist/todo/activity/MainActivity.kt
+++ app/src/main/java/com/minimalist/todo/activity/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.minimalist.todo.activity
 
-import android.annotation.TargetApi
 import android.appwidget.AppWidgetManager
-import android.content.ActivityNotFoundException
 import android.content.ComponentName
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -117,20 +114,10 @@ class MainActivity : AppCompatActivity() {
         startActivity(browserIntent)
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private fun openGooglePlayStore() {
-        val uri = Uri.parse("market://details?id=${this.packageName}")
-        val goToMarket = Intent(Intent.ACTION_VIEW, uri)
-        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
-                Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
-                Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-        try {
-            startActivity(goToMarket)
-        } catch (e: ActivityNotFoundException) {
-            startActivity(Intent(
-                    Intent.ACTION_VIEW,
-                    Uri.parse("https://play.google.com/store/apps/details?id=${this.packageName}")))
-        }
+        startActivity(Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id=${this.packageName}")))
     }
 
 }
